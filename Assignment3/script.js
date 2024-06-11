aa2d7fe28ed2d85c5526e234d9f39496bd337514
s3893888 <s3893888@student.rmit.edu.au>
2024-06-11 19:14:56 +1000

    CODE COMMENTS

--- Assignment3/script.js
+++ Assignment3/script.js
@@ -1,22 +1,19 @@
 
-// get the search tool
 const searchTool = document.querySelector("#search-tool");
 
-// add mousemove to the document so that the search tool is enable for the entire screen
 document.addEventListener("mousemove", revealImage);
 
-//change the position of the search tool based on mouse position
 function revealImage(event) {
   searchTool.style.translate = `${event.clientX}px ${event.clientY}px`;
 }
 
-// find grid items that are marked as trash
+
 const trashSpots = document.querySelectorAll(".trash");
 
 trashSpots.forEach(addHover);
 
 function addHover(spot) {
-  // add hover effect so that whenever mouse is on top of them, display a message
+
   spot.addEventListener("mouseover", function () {
     displayMessage(spot);
   });
